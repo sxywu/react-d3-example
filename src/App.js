d47eb5a180c7a515d74cae3c2aec411c3a6032c8
Shirley Wu <shirleyxueyangwu@gmail.com>
2018-04-12 16:24:48 +0200

    Update data to 2017 temps

--- src/App.js
+++ src/App.js
@@ -7,19 +7,21 @@ import BarChart from './visualizations/BarChart';
 class App extends Component {
   state = {
     temps: {},
-    city: 'sf', // city whose temperatures to show
+    city: 'am', // city whose temperatures to show
   };
 
   componentDidMount() {
     Promise.all([
       fetch(`${process.env.PUBLIC_URL}/sf.json`),
       fetch(`${process.env.PUBLIC_URL}/ny.json`),
+      fetch(`${process.env.PUBLIC_URL}/am.json`),
     ]).then(responses => Promise.all(responses.map(resp => resp.json())))
-    .then(([sf, ny]) => {
+    .then(([sf, ny, am]) => {
       sf.forEach(day => day.date = new Date(day.date));
       ny.forEach(day => day.date = new Date(day.date));
+      am.forEach(day => day.date = new Date(day.date));
 
-      this.setState({temps: {sf, ny}});
+      this.setState({temps: {sf, ny, am}});
     });
   }
 
@@ -32,13 +34,24 @@ class App extends Component {
 
     return (
       <div className="App">
-        <select name='city' onChange={this.updateCity}>
-          {
-            [{label: 'San Francisco', value: 'sf'}, {label: 'New York', value: 'ny'}].map(option => {
-              return (<option key={option.value} value={option.value}>{option.label}</option>);
-            })
-          }
-        </select>
+        <h1>
+          2017 temperatures for
+          <select name='city' onChange={this.updateCity}>
+            {
+              [
+                {label: 'Amsterdam', value: 'am'},
+                {label: 'San Francisco', value: 'sf'},
+                {label: 'New York', value: 'ny'},
+              ].map(option => {
+                return (<option key={option.value} value={option.value}>{option.label}</option>);
+              })
+            }
+          </select>
+        </h1>
+        <p>
+          *warning: these are <em>not</em> meant to be good examples of data visualizations,<br />
+          but just to show the possibility of using D3 and React*
+        </p>
         <BarChart data={data} />
         <LineChart data={data} />
       </div>
