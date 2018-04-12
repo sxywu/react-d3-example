4e6ef57d43ffea43179be09eb8eb8f0f9d9d511b
Shirley Wu <shirleyxueyangwu@gmail.com>
2018-04-12 10:52:05 +0200

    App: load in data, set up select

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 import React, { Component } from 'react';
+import {timeParse} from 'd3';
 import './App.css';
+import LineChart from './visualizations/LineChart';
 
 class App extends Component {
+  state = {
+    temps: {},
+    city: 'sf', // city whose temperatures to show
+  };
+
+  componentDidMount() {
+    Promise.all([
+      fetch(`${process.env.PUBLIC_URL}/sf.json`),
+      fetch(`${process.env.PUBLIC_URL}/ny.json`),
+    ]).then(responses => Promise.all(responses.map(resp => resp.json())))
+    .then(([sf, ny]) => {
+      sf.forEach(day => day.date = new Date(day.date));
+      ny.forEach(day => day.date = new Date(day.date));
+
+      this.setState({temps: {sf, ny}});
+    });
+  }
+
+  updateCity(e) {
+    this.setState({city: e.target.value});
+  }
+
   render() {
     return (
       <div className="App">
+        <select name='city' onChange={this.updateCity}>
+          {
+            [{label: 'San Francisco', value: 'sf'}, {label: 'New York', value: 'ny'}].map(option => {
+              return (<option key={option.value} value={option.value}>{option.label}</option>);
+            })
+          }
+        </select>
       </div>
     );
   }
