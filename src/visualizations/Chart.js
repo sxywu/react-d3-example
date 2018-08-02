055be1271fa5fba5ff4fb24570ac2a59a83ea329
Shirley Wu <shirleyxueyangwu@gmail.com>
2018-08-01 21:08:16 -0700

    Update radial chart for canvas

--- src/visualizations/Chart.js
+++ src/visualizations/Chart.js
@@ -1,31 +1,47 @@
-import React, { Component } from 'react';
-import * as d3 from 'd3';
-import chroma from 'chroma-js';
+import React, { Component } from "react";
+import * as d3 from "d3";
+import chroma from "chroma-js";
 
 const width = 650;
 const height = 650;
-const margin = {top: 20, right: 5, bottom: 20, left: 35};
-const red = '#eb6a5b';
-const green = '#b6e86f';
-const blue = '#52b6ca';
-const colors = chroma.scale([blue, green, red]);
 
 class Chart extends Component {
   state = {
+    slices: [] // array of svg path commands, each representing a day
   };
 
-  static getDerivedStateFromProps(nextProps, prevState) {
-    const {data} = nextProps;
-    if (!data) return {};
+  arcGenerator = d3.arc();
 
-    return {};
+  static getDerivedStateFromProps(nextProps, prevState) {
+    if (!nextProps.data) return null; // data hasn't been loaded yet so do nothing
+    const { data } = nextProps;
+    const radiusScale = d3.scaleLinear().range([0, width / 2]);
+    const colorScale = d3.scaleSequential(d3.interpolateRdYlBu);
+
+    // data has changed, so recalculate scale domains
+    const tempMax = d3.max(data, d => d.high);
+    const colorDomain = d3.extent(data, d => d.avg).reverse();
+    radiusScale.domain([0, tempMax]);
+    colorScale.domain(colorDomain);
+
+    // one arc per day, innerRadius is low temp, outerRadius is high temp
+    const perSliceAngle = (2 * Math.PI) / data.length;
+    const slices = data.map((d, i) => {
+      const arcData = {
+        startAngle: i * perSliceAngle,
+        endAngle: (i + 1) * perSliceAngle,
+        innerRadius: radiusScale(d.low),
+        outerRadius: radiusScale(d.high)
+      };
+
+      return { arcData, fill: colorScale(d.avg) };
+    });
+
+    return { slices };
   }
 
   render() {
-    return (
-      <svg width={width} height={height}>
-      </svg>
-    );
+    return <canvas ref="canvas" width={width} height={height} />;
   }
 }
 
--- src/visualizations/RadialChart.js
+++ src/visualizations/RadialChart.js
@@ -1,74 +1,69 @@
-import React, { Component } from 'react';
-import * as d3 from 'd3';
-import chroma from 'chroma-js';
+import React, { Component } from "react";
+import * as d3 from "d3";
 
 const width = 650;
 const height = 650;
-const red = '#eb6a5b';
-const green = '#b6e86f';
-const blue = '#52b6ca';
-const colors = chroma.scale([blue, green, red]);
 
 class RadialChart extends Component {
   state = {
-    slices: [], // array of svg path commands, each representing a day
-    tempAnnotations: [],
-    // d3 helpers
-    radiusScale: d3.scaleLinear().range([0, width / 2]),
-    colorScale: d3.scaleLinear(),
-    arcGenerator: d3.arc(),
+    slices: [] // array of svg path commands, each representing a day
   };
+
+  arcGenerator = d3.arc();
 
   static getDerivedStateFromProps(nextProps, prevState) {
     if (!nextProps.data) return null; // data hasn't been loaded yet so do nothing
-    const {data} = nextProps;
-    const {radiusScale, colorScale, arcGenerator} = prevState;
+    const { data } = nextProps;
+    const radiusScale = d3.scaleLinear().range([0, width / 2]);
+    const colorScale = d3.scaleSequential(d3.interpolateRdYlBu);
 
     // data has changed, so recalculate scale domains
     const tempMax = d3.max(data, d => d.high);
-    const colorDomain = d3.extent(data, d => d.avg);
+    const colorDomain = d3.extent(data, d => d.avg).reverse();
     radiusScale.domain([0, tempMax]);
     colorScale.domain(colorDomain);
 
     // one arc per day, innerRadius is low temp, outerRadius is high temp
     const perSliceAngle = (2 * Math.PI) / data.length;
     const slices = data.map((d, i) => {
-      const path = arcGenerator({
+      const arcData = {
         startAngle: i * perSliceAngle,
         endAngle: (i + 1) * perSliceAngle,
         innerRadius: radiusScale(d.low),
-        outerRadius: radiusScale(d.high),
-      });
+        outerRadius: radiusScale(d.high)
+      };
 
-      return {path, fill: colors(colorScale(d.avg))};
+      return { arcData, fill: colorScale(d.avg) };
     });
 
-    const tempAnnotations = [5, 20, 40, 60, 80].map(temp => {
-      return {
-        r: radiusScale(temp),
-        temp,
-      }
-    });
+    return { slices };
+  }
 
-    return {slices, tempAnnotations};
+  componentDidMount() {
+    this.ctx = this.refs.canvas.getContext("2d");
+    // and translate it so that the slices are centered
+    this.ctx.translate(width / 2, height / 2);
+    // set ctx on arc generator
+    this.arcGenerator.context(this.ctx);
+
+    this.drawSlices();
+  }
+
+  componentDidUpdate() {
+    this.drawSlices();
+  }
+
+  drawSlices() {
+    this.state.slices.forEach(slice => {
+      this.ctx.fillStyle = slice.fill;
+      this.ctx.beginPath();
+      this.arcGenerator(slice.arcData);
+      this.ctx.fill();
+    });
   }
 
   render() {
-
-    return (
-      <svg width={width} height={height}>
-        <g transform={`translate(${width / 2}, ${height / 2})`}>
-          {this.state.slices.map((d, i) => (<path key={i} d={d.path} fill={d.fill} />))}
-
-          {this.state.tempAnnotations.map((d, i) => (
-            <g key={i}>
-              <circle r={d.r} fill='none' stroke='#999' />
-              <text y={-d.r - 2} textAnchor='middle'>{d.temp}℉</text>
-            </g>
-          ))}
-        </g>
-      </svg>
-    );
+    return <canvas ref="canvas" width={width} height={height} />;
   }
 }
 
