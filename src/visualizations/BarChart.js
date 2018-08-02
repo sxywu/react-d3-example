723747a0c576563b52d857ba8b5b3b5530043fd9
Shirley Wu <shirleyxueyangwu@gmail.com>
2018-08-01 20:49:38 -0700

    Canvas bar chart ready

--- src/visualizations/BarChart.js
+++ src/visualizations/BarChart.js
@@ -1,38 +1,26 @@
-import React, { Component } from 'react';
-import * as d3 from 'd3';
-import chroma from 'chroma-js';
+import React, { Component } from "react";
+import * as d3 from "d3";
 
 const width = 650;
 const height = 400;
-const margin = {top: 20, right: 5, bottom: 20, left: 35};
-const red = '#eb6a5b';
-const green = '#b6e86f';
-const blue = '#52b6ca';
-const colors = chroma.scale([blue, green, red]).mode('hsl');
+const margin = { top: 20, right: 5, bottom: 20, left: 35 };
 
 class BarChart extends Component {
   state = {
-    bars: [], // array of rects
-    // d3 helpers
-    xScale: d3.scaleTime().range([margin.left, width - margin.right]),
-    yScale: d3.scaleLinear().range([height - margin.bottom, margin.top]),
-    colorScale: d3.scaleLinear(),
+    bars: [] // array of rects
   };
-
-  xAxis = d3.axisBottom().scale(this.state.xScale)
-    .tickFormat(d3.timeFormat('%b'));
-  yAxis = d3.axisLeft().scale(this.state.yScale)
-    .tickFormat(d => `${d}℉`);
 
   static getDerivedStateFromProps(nextProps, prevState) {
     if (!nextProps.data) return null; // data hasn't been loaded yet so do nothing
-    const {data} = nextProps;
-    const {xScale, yScale, colorScale} = prevState;
+    const { data } = nextProps;
+    const xScale = d3.scaleTime().range([margin.left, width - margin.right]);
+    const yScale = d3.scaleLinear().range([height - margin.bottom, margin.top]);
+    const colorScale = d3.scaleSequential(d3.interpolateRdYlBu);
 
     // data has changed, so recalculate scale domains
     const timeDomain = d3.extent(data, d => d.date);
     const tempMax = d3.max(data, d => d.high);
-    const colorDomain = d3.extent(data, d => d.avg);
+    const colorDomain = d3.extent(data, d => d.avg).reverse();
     xScale.domain(timeDomain);
     yScale.domain([0, tempMax]);
     colorScale.domain(colorDomain);
@@ -45,29 +33,40 @@ class BarChart extends Component {
         x: xScale(d.date),
         y: y1,
         height: y2 - y1,
-        fill: colors(colorScale(d.avg)),
-      }
+        fill: colorScale(d.avg)
+      };
     });
 
-    return {bars};
+    return { bars };
+  }
+
+  componentDidMount() {
+    this.ctx = this.refs.canvas.getContext("2d");
+    this.drawBars();
   }
 
   componentDidUpdate() {
-    d3.select(this.refs.xAxis).call(this.xAxis);
-    d3.select(this.refs.yAxis).call(this.yAxis);
+    this.drawBars();
+  }
+
+  drawBars() {
+    this.state.bars.forEach(bar => {
+      this.ctx.fillStyle = bar.fill;
+      this.ctx.fillRect(bar.x, bar.y, 2, bar.height);
+    });
   }
 
   render() {
-
     return (
-      <svg width={width} height={height}>
-        {this.state.bars.map((d, i) =>
-          (<rect key={i} x={d.x} y={d.y} width='2' height={d.height} fill={d.fill} />))}
-        <g>
-          <g ref='xAxis' transform={`translate(0, ${height - margin.bottom})`} />
-          <g ref='yAxis' transform={`translate(${margin.left}, 0)`} />
-        </g>
-      </svg>
+      <canvas
+        ref="canvas"
+        width={width}
+        height={height}
+        style={{
+          width: `${width}px`,
+          height: `${height}px`
+        }}
+      />
     );
   }
 }
--- src/visualizations/Chart.js
+++ src/visualizations/Chart.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import * as d3 from "d3";
-import chroma from "chroma-js";
 
 const width = 650;
 const height = 400;
@@ -10,14 +9,47 @@ class Chart extends Component {
   state = {};
 
   static getDerivedStateFromProps(nextProps, prevState) {
+    if (!nextProps.data) return null; // data hasn't been loaded yet so do nothing
     const { data } = nextProps;
-    if (!data) return {};
+    const xScale = d3.scaleTime().range([margin.left, width - margin.right]);
+    const yScale = d3.scaleLinear().range([height - margin.bottom, margin.top]);
+    const colorScale = d3.scaleSequential(d3.interpolateRdYlBu);
 
-    return {};
+    // data has changed, so recalculate scale domains
+    const timeDomain = d3.extent(data, d => d.date);
+    const tempMax = d3.max(data, d => d.high);
+    const colorDomain = d3.extent(data, d => d.avg).reverse();
+    xScale.domain(timeDomain);
+    yScale.domain([0, tempMax]);
+    colorScale.domain(colorDomain);
+
+    // calculate x and y for each rectangle
+    const bars = data.map(d => {
+      const y1 = yScale(d.high);
+      const y2 = yScale(d.low);
+      return {
+        x: xScale(d.date),
+        y: y1,
+        height: y2 - y1,
+        fill: colorScale(d.avg)
+      };
+    });
+
+    return { bars };
   }
 
   render() {
-    return <svg width={width} height={height} />;
+    return (
+      <canvas
+        ref="canvas"
+        width={width}
+        height={height}
+        style={{
+          width: `${width}px`,
+          height: `${height}px`
+        }}
+      />
+    );
   }
 }
 
