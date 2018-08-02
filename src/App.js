87f2570e59b048bbadb9bb8836833b8bc2e15d53
Shirley Wu <shirleyxueyangwu@gmail.com>
2018-08-01 18:42:50 -0700

    Update brush branch

--- src/App.js
+++ src/App.js
@@ -1,39 +1,38 @@
-import React, { Component } from 'react';
-import './App.css';
-import LineChart from './visualizations/LineChart';
-import BarChart from './visualizations/BarChart';
-import RadialChart from './visualizations/RadialChart';
-import Chart from './visualizations/Chart';
+import React, { Component } from "react";
+import "./App.css";
+import LineChart from "./visualizations/LineChart";
+import BarChart from "./visualizations/BarChart";
+import RadialChart from "./visualizations/RadialChart";
+import Chart from "./visualizations/Chart";
 
 class App extends Component {
   state = {
     temps: {},
-    city: 'sf', // city whose temperatures to show
-    range: [], // time range set by the brush
+    city: "sf", // city whose temperatures to show
+    range: [] // time range set by the brush
   };
 
   componentDidMount() {
     Promise.all([
-      fetch(`${process.env.PUBLIC_URL}/sf.json`),
-      fetch(`${process.env.PUBLIC_URL}/ny.json`),
-      fetch(`${process.env.PUBLIC_URL}/am.json`),
-    ]).then(responses => Promise.all(responses.map(resp => resp.json())))
-    .then(([sf, ny, am]) => {
-      sf.forEach(day => day.date = new Date(day.date));
-      ny.forEach(day => day.date = new Date(day.date));
-      // am.forEach(day => day.date = new Date(day.date));
+      fetch(`${process.env.PUBLIC_URL || ""}/sf.json`),
+      fetch(`${process.env.PUBLIC_URL || ""}/ny.json`)
+    ])
+      .then(responses => Promise.all(responses.map(resp => resp.json())))
+      .then(([sf, ny]) => {
+        sf.forEach(day => (day.date = new Date(day.date)));
+        ny.forEach(day => (day.date = new Date(day.date)));
 
-      this.setState({temps: {sf, ny, am}});
-    });
+        this.setState({ temps: { sf, ny } });
+      });
   }
 
-  updateCity = (e) => {
-    this.setState({city: e.target.value});
-  }
+  updateCity = e => {
+    this.setState({ city: e.target.value });
+  };
 
-  updateRange = (range) => {
-    this.setState({range});
-  }
+  updateRange = range => {
+    this.setState({ range });
+  };
 
   render() {
     const data = this.state.temps[this.state.city];
@@ -42,28 +41,38 @@ class App extends Component {
       <div className="App">
         <h1>
           2017 Temperatures for
-          <select name='city' onChange={this.updateCity}>
-            {
-              [
-                {label: 'San Francisco', value: 'sf'},
-                {label: 'New York', value: 'ny'},
-                  // {label: 'Amsterdam', value: 'am'},
-              ].map(option => {
-                return (<option key={option.value} value={option.value}>{option.label}</option>);
-              })
-            }
+          <select name="city" onChange={this.updateCity}>
+            {[
+              { label: "San Francisco", value: "sf" },
+              { label: "New York", value: "ny" }
+              // {label: 'Amsterdam', value: 'am'},
+            ].map(option => {
+              return (
+                <option key={option.value} value={option.value}>
+                  {option.label}
+                </option>
+              );
+            })}
           </select>
         </h1>
         <p>
-          *warning: these are <em>not</em> meant to be good examples of data visualizations,<br />
+          *warning: these are <em>not</em> meant to be good examples of data
+          visualizations,<br />
           but just to show the possibility of using D3 and React*
         </p>
-        <BarChart data={data} range={this.state.range} updateRange={this.updateRange} />
+        <BarChart
+          data={data}
+          range={this.state.range}
+          updateRange={this.updateRange}
+        />
         <RadialChart data={data} range={this.state.range} />
         <Chart data={data} />
 
         <p>
-          (Weather data from <a href='wunderground.com' target='_new'>wunderground.com</a>)
+          (Weather data from{" "}
+          <a href="wunderground.com" target="_new">
+            wunderground.com
+          </a>)
         </p>
       </div>
     );
