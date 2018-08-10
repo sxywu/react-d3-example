83c98c78c898ee6d9a75e6342940c469dc84d4c7
Shirley Wu <shirleyxueyangwu@gmail.com>
2018-08-10 08:24:07 -0700

    Get rid of chroma

--- src/visualizations/Chart.js
+++ src/visualizations/Chart.js
@@ -1,14 +1,9 @@
 import React, { Component } from 'react';
 import * as d3 from 'd3';
-import chroma from 'chroma-js';
 
 const width = 650;
 const height = 650;
 const margin = {top: 20, right: 5, bottom: 20, left: 35};
-const red = '#eb6a5b';
-const green = '#b6e86f';
-const blue = '#52b6ca';
-const colors = chroma.scale([blue, green, red]);
 
 class Chart extends Component {
   state = {
