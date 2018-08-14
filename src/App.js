01e257695223c1dc2c7a24309e7a9131db6a3d02
Shirley Wu <shirleyxueyangwu@gmail.com>
2018-08-14 14:43:02 -0700

    Save a better version of the movies

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import _ from "lodash";
-import inflation from "us-inflation";
 
 const startYear = 2013;
 const numYears = 5;
@@ -16,18 +15,8 @@ class App extends Component {
       .then(resp => resp.json())
       .then(movies => {
         movies = _.chain(movies)
-          .map(d => {
-            const year = +d.Year;
-            const date = new Date(d.Released);
-            const boxOffice = parseInt(d.BoxOffice.replace(/[\$\,]/g, ""));
-            return {
-              title: d.Title,
-              date,
-              boxOffice: boxOffice && inflation({ year, amount: boxOffice }),
-              score: +d.Metascore,
-              year
-            };
-          }).filter(d => d.boxOffice && d.year >= startYear)
+          .map(d => Object.assign(d, {date: new Date(d.date)}))
+          .filter(d => d.boxOffice && d.year >= startYear)
           .value();
         this.setState({movies});
       });
